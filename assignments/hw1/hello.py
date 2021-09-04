6e64fc73fc12a1d5988d07bd015f7af3d9ddb6a8
Belinda Trinh <trinhb@g.cofc.edu>
2021-09-03 22:58:51 -0400

    completed hw 1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,15 @@
+"""
+Name : Belinda Trinh
+hello.py
+Problem: print Hello,world! to the screen
+Certification of Authenticity
+I certify that this assignment is entirely my own work
+
+"""
+
+
+def main():
+    print("hello, world!")
+
+
+main()
