45829aab4d35edd2e1f9fdc6fc193c4dda036a21
Belinda Trinh <trinhb@g.cofc.edu>
2021-09-17 11:12:41 -0400

    completed hw2

---
+++ assignments/hw2/chapter4.py
@@ -0,0 +1,23 @@
+""""
+Name : Belinda Trinh
+calculate.py
+Problem: a place to play with chpater 4 material
+Certification of Authenticity
+I certify that this assignment is entirely my own work
+
+"""
+from graphics import *
+
+
+
+win = GraphWin("My Window", 700, 500)
+
+pointA= Point(350, 250)
+pointB= Point(100, 50)
+
+circle A = Circle(point A, 50)
+
+pointA.draw(win)
+pointB.draw(win)
+
+circle A= Circle(pointA, 50)
