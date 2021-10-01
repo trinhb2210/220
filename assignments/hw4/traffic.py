bb9168797837bbb5a4011472bdf71b5698706849
Belinda Trinh <trinhb@g.cofc.edu>
2021-09-30 23:39:42 -0400

    completed hw4

--- assignments/hw4/traffic.py
+++ assignments/hw4/traffic.py
@@ -1,26 +1,28 @@
 """
 Name : Belinda Trinh
 traffic.py
-Problem: This program calculates the
+Problem: This program calculates the average of vehicles that travel each road for each day.
+Certification of Authenticity
 I certify that this assignment is entirely my own work
 
 """
 
 
 def main():
-    roads = eval(input(" how many roads were surveyed? :"))
+    number_of_roads = eval(input(" how many roads were surveyed? :"))
     total_vehicles = 0
-    for road in range(1, roads + 1):
-        number = eval(input(" how many days was road "+str(road) + " surveyed ?"))
+    for road in range(1, number_of_roads + 1):
+        number_of_days = eval(input(" how many days was road "+str(road) + " surveyed ?"))
         acc = 0
-        for day in range(1, number + 1):
-            cars_per_day = eval(input("how many cars travelled on day" + str(day) + ":"))
+        for day in range(1, number_of_days + 1):
+            cars_per_day = eval(input("how many cars travelled on day "+str(day)+"?"))
             acc = acc + cars_per_day
-        avg = acc/ number
-        print(" Road", road, "average vehicles per day", round(avg, 2))
+        average_cars = acc / number_of_days
+        print(" Road", road, "average vehicles per day", round(average_cars, 2))
         total_vehicles = acc + total_vehicles
-    average_vehicles = total_vehicles/ roads
+    average_vehicles = total_vehicles/ number_of_roads
     print("Total number of vehicles traveled on all roads : ", total_vehicles)
-    print("Average number of vehicles per road:", round(average_vehicles,2))
+    print("Average number of vehicles per road:", round(average_vehicles, 2))
+if __name__ == '__main__':
 
-
+    main()
