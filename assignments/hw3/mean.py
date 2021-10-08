5a288faec8d95878959744dc2cb87c423b7f7f40
Belinda Trinh <trinhb@g.cofc.edu>
2021-10-07 23:36:06 -0400

    completed hw 3

--- assignments/hw3/mean.py
+++ assignments/hw3/mean.py
@@ -9,28 +9,27 @@ import math
 
 
 def main():
-    acc = 0
-    x = eval(input("Number of values to be entered: "))
-    for i in range(1, x + 1):
-        y = eval(input("Enter your value: "))
-        acc = acc + (y * y)
-    mean = float(acc) / x
+    rms_acc = 0
+    harmonic_acc = 0
+    geometric_acc = 1
+    number = eval(input("Number of values to be entered: "))
+    for i in range(1, number + 1):
+        value = eval(input("Enter your value: "))
+        rms_acc = rms_acc + (value * value)
+        harmonic_acc = harmonic_acc + (1 / value)
+        geometric_acc = geometric_acc * value
+    mean = float(rms_acc) / number
     rms_average = math.sqrt(mean)
+    harmonic_mean = number / harmonic_acc
+    geometric_mean = math.pow(geometric_acc, 1 / number)
     print(round(rms_average, 3))
+    print(round(harmonic_mean, 3))
+    print(round(geometric_mean, 3))
 
-    acc=0
-    for i in range(1, x +1):
-        y = eval(input("Enter your value: "))
-        acc = acc + (1 / y)
-    harmonic_mean = x / acc
-    print(round(harmonic_mean,3))
 
-    acc=1
-    for i in range(1,x +1):
-        y = eval(input("Enter your value: "))
-        acc = acc * y
-    geometric_mean= math.pow(acc, 1/x)
-    print(round(geometric_mean,3))
+if __name__ == '__main__':
+
+    main()
 
 
 
