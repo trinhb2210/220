df79fd158d717272f58b22723f33e0e2d216a039
Belinda Trinh <trinhb@g.cofc.edu>
2021-10-11 15:35:15 -0400

    completed lab 7

--- labs/lab7/lab7.py
+++ labs/lab7/lab7.py
@@ -1,13 +1,57 @@
 """
-Name: <your name goes here – first and last>
+Name: <Belinda Trinh>
 Partner: <your partner's name goes here – first and last>
-<ProgramName>.py
+<lab6>.py
 """
+import math
+
+def cash_conversion():
+    i = eval(input(" Enter the integer : "))
+    print("${:.2f}".format(i))
+def encode():
+    x = input("integer key value")
+    k = eval(input(" enter message to be encoded :  "))
+    acc = ""
+    for c in x:
+        i = ord(c)
+        new_character = chr(i + k)
+        acc = acc + new_character
+    print(acc)
+
+def sphere_area(radius):
+    return ( 4 * math.pi * radius ** 3)
+def sphere_volume(radius):
+    return((4/3) * math.pi * radius ** 3)
+def sum_n(n):
+    acc = 0
+    for i in range (1, n +1):
+        acc+= i
+    return acc
+def sum_n_cubes(n):
+    acc = 0
+    for i in range(1, n+1):
+        acc+= i * i * i
+    return acc
+def encode_better():
+    m = input(" message to be encoded : ")
+    n = input(" enter the cipher key: ")
+    acc = ""
+    for i in range(len(m)):
+        c = ord(m[i])
+        key = (ord(k[i])
+
+
+
+
+
 
 
 def main():
-    # add function calls here
-    pass
-
+    cash_conversion()
+    print(sphere_area(5))
+    print(sphere_volume(2))
+    print(sum_n(10))
+    print(sum_n_cubes(10))
+    encode()
 
 main()
