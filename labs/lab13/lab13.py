0e44a6f874650509c71b56faac1e9d600a556390
Belinda Trinh <trinhb@g.cofc.edu>
2021-11-29 14:55:39 -0500

    completed lab 13

--- labs/lab13/lab13.py
+++ labs/lab13/lab13.py
@@ -1,11 +1,30 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Belinda Trinh
+lab13.py
 """
 
-def main():
-    # add other function calls here
-    pass
+def binary(list, value):
+    mid = list[len(list)//2]
+    while mid != value and len(list) != 1:
+        if mid > value:
+            list = list[:]
+        else:
+            list = list[:]
+        mid = list[len(list)//2]
+    if len(list) == 1 and list[0] != value:
+        return False
+    else:
+        return True
+
+def selection_sort(values):
+    for i in range (len(values)):
+        lowest = values [i]
+        pos = i
+        for j in range ( i + 1, len(values)):
+            if values[j] < values[lowest]:
+                lowest = values[j]
+                pos = j
+        values[i], values[pos] = values[pos], values[i]
 
 
 main()
